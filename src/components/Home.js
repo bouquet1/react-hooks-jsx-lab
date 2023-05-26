3de3ec96c86ab8075eace9434ad90cdd012855f3
bouquet1 <bukuleta@gmail.com>
2023-05-25 18:56:21 -0700

    passes test

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { name, city } from "../data/data.js";
+import NavBar from "./NavBar.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      {/* <NavBar /> */}
+      <h1>
+        {name} is a Web Developer {city}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
