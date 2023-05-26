de57f3e6579fd46369c73764a7a00ce68a8778dc
bouquet1 <bukuleta@gmail.com>
2023-05-25 19:18:40 -0700

    passes all tests

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home() {
   return (
     <div id="home">
       <h1 style={firebrick}>
-        {name} is a Web Developer {city}
+        {name} is a Web Developer from {city}
       </h1>
     </div>
   );
